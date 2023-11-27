ef1dfabdb84f90c953201ccfd0bb5c8c4c500b1f
NathanHGayle <nathanhg5@gmail.com>
2023-11-27 21:46:37 +0000

    most recent update

---
+++ Preparation & Cleaning Redundancies/GitIgnore/Continued Feature Selection.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv()
+
+rename variables to 'Residual operational categories'
+# The data is grouped by consituency group.  With con being the % of adult % when grouped.
+
+# Create ID and group by whatever you feel appropriate to join .
+
+# I want figures for each socio-economic group by post code
